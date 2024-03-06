8ae7e886ee1087b36829afb1ab598f15dd1aabc2
HeatherCurrie <heathercurrie1000@gmail.com>
2024-03-06 22:00:37 +0000

    Export working - Needs slight updates on UI

--- Front/JS/management.js
+++ Front/JS/management.js
@@ -1,3 +1,54 @@
+function dropdownOptions() {
+    fetch('http://127.0.0.1:5000/get-all-reviews')
+    .then(response => response.json())
+    .then(data => {
+        const select = document.getElementById('feedbackDropdown');
+
+        data.forEach(option => {
+            if (option.completed === "Completed"){
+                const opt = document.createElement('option');
+                opt.value = option.feedbackID;
+                opt.textContent = option.moduleName; 
+                select.appendChild(opt);
+            }
+        });
+    })
+    .catch(error => console.error('Error fetching options:', error));
+}
+
+function exportWord() {
+    document.getElementById("exportButton").innerHTML = '<i class="fa-solid fa-gear fa-spin" id="spinner"></i>'
+    const select = document.getElementById("feedbackDropdown");
+    const selectedVal = Array.from(select.selectedOptions).map(option => option.value);
+        
+    const data = {
+        feedbackID: selectedVal
+    };
+        
+    // Send the data to the server using Fetch API
+    fetch('http://127.0.0.1:5000/export-word', { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok: ' + response.statusText);
+        }
+        return response.json(); 
+    })
+    .then(data => {
+        //window.location.href = 'management.html';
+        console.log('Success:', data); 
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        document.getElementById("exportButton").innerHTML = 'Export Failed'; // Inform the user
+    });
+}
+
 function displayIncompleteReviews() {
     fetch('http://127.0.0.1:5000/get-all-reviews')
         .then(response => response.json())  // Parse the JSON from the response
@@ -5,8 +56,16 @@ function displayIncompleteReviews() {
             document.getElementById("incompleteSpinner").remove()
             const incompleteTable = document.getElementById('incompleteTable');
 
+            let incompleteReviews = 0;
+
             // Check if there are any reviews
-            if (data.length === 0) {
+            data.forEach(review => {
+                if (review.completed === "To-Complete" || review.completed ==="In-Progress") {
+                    incompleteReviews += 1;
+                }
+            })
+
+            if (data.length === 0 || incompleteReviews === 0) {
                 incompleteTable.innerHTML = '<tr><td colspan="3" class="text-center">No reviews found</td></tr>';
                 return;
             }
@@ -20,7 +79,7 @@ function displayIncompleteReviews() {
 
                 // Create a table row and cells
                 let row;
-                if (review.completed === 'To-Complete' || review.completed ==='In-Progress') {
+                if (review.completed === "To-Complete" || review.completed ==="In-Progress") {
                     row = incompleteTable.insertRow();
                 } 
 
@@ -41,4 +100,5 @@ function displayIncompleteReviews() {
         });
 }
 // Call the function when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', dropdownOptions);
 document.addEventListener('DOMContentLoaded', displayIncompleteReviews);
